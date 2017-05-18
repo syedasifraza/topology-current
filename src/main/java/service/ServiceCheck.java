dec09ff9cba0700e49a8f73c9fe5044a456258e8
asifraza <syedasif120683@gmail.com>
2017-05-18 01:45:07 -0400

    solve single switch path problem + enable meters settings

--- src/main/java/service/ServiceCheck.java
+++ src/main/java/service/ServiceCheck.java
@@ -93,6 +93,11 @@ public class ServiceCheck {
     @Deactivate
     protected void deactivate() {
         agentFlowService.removeFlowsByAppId();
+        rmqService.removeListener(rmqMsgListener);
+        configService.removeListener(configListener);
+        eventExecutor.shutdownNow();
+        eventExecutor = null;
+
         log.info("Stopped");
     }
 
